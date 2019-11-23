f19ebc885b54ba9dbf518164448e0f2b05cb3591
HempSheet <58041457+HempSheet@users.noreply.github.com>
2019-11-23 23:52:51 +0200

    ADD
    ADD

---
+++ ths2.py
@@ -0,0 +1,48 @@
+import asyncio as asio
+from aio_pika import connect, IncomingMessage, Message, exchange
+
+
+class Resiver():
+
+    def __init__(self,  queue='hello', host='192.168.1.8', routingKey='hello', exchange=''):
+        self.queue = queue
+        self.host = host
+        self.connection = None
+        self.channel = None
+        self.callback_queue = None
+        self.exchange = exchange
+        self.routingKey = routingKey
+
+class Rabbitmq():
+
+    def __init__(self, server):
+# добавить def main для асинхрон
+        self.server = server
+        self.connection = await connect(host=self.server.host,
+                                        port=5672,
+                                        login='admin',
+                                        password='admin',
+                                        virtual_host='/',
+                                        loop=loop
+                                        )
+        self.channel = await self.connection.channel()
+
+    def setup(self, payload={}):
+        self.channel.queue_declare(queue=self.queue)
+        self.channel.basic_publish(exchange=self.server.exchange,
+                                   routing_key='hello',
+                                   body=str(payload)
+                                   )
+        print("[x] Sent 'Hello!'")
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    server = Resiver(queue='hello',
+                     host='192.168.1.8',
+                     routingKey='hello',
+                     exchange='')
+    rabbitmq = Rabbitmq(server)
+    rabbitmq.setup(payload={"Data:22"})
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
